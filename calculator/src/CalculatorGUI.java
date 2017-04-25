02417c37bdeac5beb238244f8f44218345876115
Andrew Odendaal <andrew@hsmoore.com>
2017-04-26 00:48:51 +0100

    Make elements private. Add Key Events and Listeners.

--- calculator/src/CalculatorGUI.java
+++ calculator/src/CalculatorGUI.java
@@ -7,16 +7,41 @@ import java.awt.event.WindowEvent;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class CalculatorGUI extends JFrame implements Calculate{
+public class CalculatorGUI extends JFrame implements Calculate {
 
     private JFrame mainFrame;
     private JPanel controlPanel;
     private JTextField mainText;
-    
+
+    private Container mainCalcContainer;
+
+    private JButton n1;
+    private JButton n2;
+    private JButton n3;
+    private JButton n4;
+    private JButton n5;
+    private JButton n6;
+    private JButton n7;
+    private JButton n8;
+    private JButton n9;
+    private JButton ndiv;
+    private JButton nmult;
+    private JButton nplus;
+    private JButton ndot;
+    private JButton n0;
+    private JButton nequal;
+    private JButton nminus;
+    private JButton nclear;
+    private JButton nfibonacci;
+
     public CalculatorGUI() {
         renderWindowComponents();
         createCalculatorButtons();
+        addButtonsEventHandlers();
+        addKeyboardListeners();
     }
 
     public void calculateNow(){
@@ -24,11 +49,11 @@ public class CalculatorGUI extends JFrame implements Calculate{
         ScriptEngineManager mgr = new ScriptEngineManager();
         ScriptEngine engine = mgr.getEngineByName("JavaScript");
         try {
-                String s = engine.eval(global).toString();
-                mainText.setText(s);
-            } catch (ScriptException e1) {
-                e1.printStackTrace();
-            }
+            String s = engine.eval(global).toString();
+            mainText.setText(s);
+        } catch (ScriptException e1) {
+            e1.printStackTrace();
+        }
     }
     
     private void addToCalculationString(String whatToAdd){
@@ -56,7 +81,6 @@ public class CalculatorGUI extends JFrame implements Calculate{
     }
 
     public void createCalculatorButtons() {
-        //TODO: Create the buttons
         /**
          +---------------------+
          | 7  |  8  |  9  | /  |
@@ -68,36 +92,34 @@ public class CalculatorGUI extends JFrame implements Calculate{
          | .  |  0  |  =  | -  |
          +---------------------+
          */
-        // ^ what other buttons do we need in the initial copy?
-        // ^ we can add the fibonacci etc. later
-        
-        Container mainCalcContainer = mainFrame.getContentPane();
-        mainText = new JTextField(100);
-        Font myFontSize = mainText.getFont().deriveFont(Font.BOLD,50f);
-        mainText.setFont(myFontSize);
-        mainCalcContainer.add(mainText,BorderLayout.NORTH);
 
-//Define each jButton
-        final JButton  n1 =  new JButton("1");
-        final JButton  n2 =  new JButton("2");
-        final JButton  n3 =  new JButton("3");
-        final JButton  n4 =  new JButton("4");
-        final JButton  n5 =  new JButton("5");
-        final JButton  n6 =  new JButton("6");
-        final JButton  n7 =  new JButton("7");
-        final JButton  n8 =  new JButton("8");
-        final JButton  n9 =  new JButton("9");
-        final JButton  ndiv =  new JButton("/");
-        final JButton  nmult =  new JButton("*");
-        final JButton  nplus =  new JButton("+");
-        final JButton  ndot =  new JButton(".");
-        final JButton  n0 =  new JButton("0");
-        final JButton  nequal =  new JButton("=");
-        final JButton  nminus =  new JButton("-");
-        final JButton  nclear =  new JButton("C");
-        final JButton  nfibonacci =  new JButton("FIB");
-        
-//Add all buttons to the jPanel
+        mainCalcContainer = mainFrame.getContentPane();
+        mainText = new JTextField(100);
+        Font myFontSize = mainText.getFont().deriveFont(Font.BOLD, 50f);
+        mainText.setFont(myFontSize);
+        mainCalcContainer.add(mainText, BorderLayout.NORTH);
+
+        // Initialise each jButton
+        n1 = new JButton("1");
+        n2 = new JButton("2");
+        n3 = new JButton("3");
+        n4 = new JButton("4");
+        n5 = new JButton("5");
+        n6 = new JButton("6");
+        n7 = new JButton("7");
+        n8 = new JButton("8");
+        n9 = new JButton("9");
+        ndiv = new JButton("/");
+        nmult = new JButton("*");
+        nplus = new JButton("+");
+        ndot = new JButton(".");
+        n0 = new JButton("0");
+        nequal = new JButton("=");
+        nminus = new JButton("-");
+        nclear = new JButton("C");
+        nfibonacci = new JButton("FIB");
+
+        // Add all buttons to the jPanel
         controlPanel.add(n7);
         controlPanel.add(n8);
         controlPanel.add(n9);
@@ -116,129 +138,198 @@ public class CalculatorGUI extends JFrame implements Calculate{
         controlPanel.add(nminus);
         controlPanel.add(nclear);
         controlPanel.add(nfibonacci);
-        
-// Add ActionListener events to all the buttons
-        n1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+
+    }
+
+    /**
+     * Add ActionListener events to all the buttons
+     */
+    public void addButtonsEventHandlers() {
+        n1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n1.getText());
             }
         });
    
-        n2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n2.getText());
             }
         });
 
    
-        n3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n3.getText());
             }
         });
    
-        n4.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n4.getText());
             }
         });
    
-        n5.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n5.getText());
             }
         });
    
-        n6.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n6.getText());
             }
         });
    
-        n7.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n7.getText());
             }
         });
    
-        n8.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n8.getText());
             }
         });
    
-        n9.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n9.getText());
             }
         });
 
-        n0.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        n0.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(n0.getText());
             }
         });
                 
-        ndiv.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        ndiv.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(ndiv.getText());
             }
         });
    
-        nmult.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        nmult.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(nmult.getText());
             }
         });
    
-        nminus.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        nminus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(nminus.getText());
             }
         });
         
-        ndot.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        ndot.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(ndot.getText());
             }
         });
         
-        nplus.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        nplus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 addToCalculationString(nplus.getText());
             }
         });      
 
-        nclear.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        nclear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 mainText.setText("");
             }
         }); 
         
-        nequal.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e)
-            {
+        nequal.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 calculateNow();
             }
         });        
         
         mainCalcContainer.add(controlPanel,BorderLayout.CENTER);
-        mainFrame.setVisible(true);  
+        mainFrame.setVisible(true);
+    }
+
+    /**
+     * addKeyboardListeners
+     */
+    private void addKeyboardListeners() {
+        mainFrame.addKeyListener(new KeyListener(){
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_UP){
+                    System.out.println("Hi");
+                }
+
+                switch(e.getKeyCode()) {
+                    case KeyEvent.VK_0: case KeyEvent.VK_NUMPAD0:
+                        n0.doClick();
+                        break;
+                    case KeyEvent.VK_1: case KeyEvent.VK_NUMPAD1:
+                        n1.doClick();
+                        break;
+                    case KeyEvent.VK_2: case KeyEvent.VK_NUMPAD2:
+                        n2.doClick();
+                        break;
+                    case KeyEvent.VK_3: case KeyEvent.VK_NUMPAD3:
+                        n3.doClick();
+                        break;
+                    case KeyEvent.VK_4: case KeyEvent.VK_NUMPAD4:
+                        n4.doClick();
+                        break;
+                    case KeyEvent.VK_5: case KeyEvent.VK_NUMPAD5:
+                        n5.doClick();
+                        break;
+                    case KeyEvent.VK_6: case KeyEvent.VK_NUMPAD6:
+                        n6.doClick();
+                        break;
+                    case KeyEvent.VK_7: case KeyEvent.VK_NUMPAD7:
+                        n7.doClick();
+                        break;
+                    case KeyEvent.VK_8: case KeyEvent.VK_NUMPAD8:
+                        n8.doClick();
+                        break;
+                    case KeyEvent.VK_9: case KeyEvent.VK_NUMPAD9:
+                        n9.doClick();
+                        break;
+
+                    case KeyEvent.VK_PLUS: case KeyEvent.VK_ADD:
+                        nplus.doClick();
+                        break;
+                    case KeyEvent.VK_MINUS: case KeyEvent.VK_SUBTRACT:
+                        nminus.doClick();
+                        break;
+                    case KeyEvent.VK_SLASH: // ?
+                        ndiv.doClick();
+                        break;
+                    case KeyEvent.VK_ASTERISK: case KeyEvent.VK_MULTIPLY:
+                        nmult.doClick();
+                        break;
+                    case KeyEvent.VK_EQUALS: case KeyEvent.VK_ENTER:
+                        nequal.doClick();
+                        break;
+                    case KeyEvent.VK_STOP: // ?
+                        ndot.doClick();
+                        break;
+
+                    case KeyEvent.VK_ESCAPE:
+                        nclear.doClick();
+                        break;
+                }
+
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+//                throw new UnsupportedOperationException("Not supported yet.");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+//                throw new UnsupportedOperationException("Not supported yet.");
+            }
+        });
     }
 }
